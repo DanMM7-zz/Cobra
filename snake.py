30586706fa6537aac28f3f8760fd18c21148619a
iamdaniko <danmalengela@yahoo.com>
2021-03-21 16:34:43 +0200

    Added window background color and size

--- snake.py
+++ snake.py
@@ -1,6 +1,16 @@
 import tkinter as tk
 
+class Snake(tk.Canvas):
+    def __init__(self):
+        super().__init__(width=600, height=620, background="brown", highlightthickness=0)
+
+
 root = tk.Tk()
+root.title("Cobra")
+root.resizable(False, False)
+
+board = Snake()
+board.pack()
 
 root.mainloop()
 
